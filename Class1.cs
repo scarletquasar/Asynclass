dffcd4e3fe95d574a9f94b668bf0a8dc21e5772a
Victória Rose <70824102+victoriaquasar@users.noreply.github.com>
2022-10-26 18:01:11 -0300

    feat: implemented Init/Catcher pattern

--- Class1.cs
+++ Class1.cs
@@ -13,12 +13,19 @@ public class Test : Async<Test>
 
     public Test(string potato, string banana)
     {
-        Setup(async instance =>
+        SetRetryTimes(10);
+
+        Init(async instance =>
         {
             await Task.Delay(1);
 
             instance.Potato = potato;
             instance.Banana = banana;
+        });
+
+        Catch(errors =>
+        {
+            errors.ForEach(error => Console.WriteLine(error.Message));
         });
     }
 }
@@ -28,10 +35,45 @@ namespace Asynclass
     public abstract class Async<TBase>
     {
         private Action<TBase> _initializer = async a => await Task.Delay(0);
+        private Action<List<Exception>> _catcher = async a => await Task.Delay(0);
+        private bool _initialized;
+        private int _retryTimes = 1;
 
-        public void Setup(Action<TBase> initializer)
+        public void SetRetryTimes(int retryTimes)
         {
-            _initializer = initializer;
+            if (!_initialized)
+            {
+                _retryTimes = retryTimes;
+
+                return;
+            }
+
+            throw new InvalidOperationException("Can't set retry times in a initialized async class");
+        }
+
+        public void Init(Action<TBase> initializer)
+        {
+            if(!_initialized)
+            {
+                _initializer = initializer;
+                _initialized = true;
+
+                return;
+            }
+
+            throw new InvalidOperationException("The class has already been initialized");
+        }
+
+        public void Catch(Action<List<Exception>> catcher)
+        {
+            if(_initialized)
+            {
+                _catcher = catcher;
+
+                return;
+            }
+
+            throw new InvalidOperationException("Can't define a catcher in a non-initialized async class");
         }
 
         private async Task<TBase> Initialize(Action<TBase> initializer)
@@ -44,7 +86,36 @@ namespace Asynclass
 
         public TaskAwaiter<TBase> GetAwaiter()
         {
-            return Initialize(_initializer).GetAwaiter();
+            TaskAwaiter<TBase>? awaiter = null;
+            List<Exception> exceptions = new();
+
+            while(awaiter is null && _retryTimes > 0)
+            {
+                try
+                {
+                    awaiter = Initialize(_initializer).GetAwaiter();
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                }
+                finally
+                {
+                    _retryTimes--;
+                }
+            }
+            
+            try
+            {
+                if (awaiter is null)
+                    throw new ArgumentNullException();
+            }
+            catch
+            {
+                _catcher(exceptions);
+            }
+
+            return (TaskAwaiter<TBase>)awaiter!;
         }
     }
 }
